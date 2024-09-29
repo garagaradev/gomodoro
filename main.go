77941fc057f0d1d275f3af43ea682fdf14fd7d33
garagaradev <garagaradev@proton.me>
2024-09-29 17:32:58 +0700

    modified main function

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
-import "github.com/garagaradev/gomodoro/gomodoro"
+import (
+  "fmt"
+  "os"
+  "strconv"
+  "github.com/garagaradev/gomodoro/gomodoro")
 
 func main(){
-  g := gomodoro.NewGomodoro()
+  if len(os.Args) < 2 {
+    fmt.Println("Please provide work duration in minutes.")
+    return
+  }
+  workDurationInput := os.Args[1]
+  workDurationMinutes, err := strconv.Atoi(workDurationInput)
+  if err != nil {
+    fmt.Println("Invalid duration:", err)
+    return
+  }
+  g := gomodoro.NewGomodoro(workDurationMinutes)
   g.StartGomodoro()
 }
